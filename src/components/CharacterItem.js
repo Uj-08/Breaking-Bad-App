4300678ba5e65ee095ece53c8321bf5789b4bb1d
Ujjwal Pandey <ujjwalpandey24@gmail.com>
2022-10-18 23:45:28 +0530

    CSS

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -1,20 +1,22 @@
-
 const CharacterItem = ({item}) => {
-
+  const {name, img, portrayed, category, appearance, status} = item
   console.log(item)
 
   return (<div className="card">
     <div className="card-inner">
       <div className="card-image">
-        <img src={item.img} alt={item.name}></img>
+        <img src={img} alt={name} onError={(e) => {
+          e.target.src = 'https://vignette.wikia.nocookie.net/breaking-bad-tv/images/c/ce/Sp.png/revision/latest?cb=20121016143623'
+        }}/>
         <div className="card-text">
-          <h1>{item.name}</h1>
+          <h1>{name}</h1>
           <ul>
-            <li><strong>Actor Name: </strong> {item.portrayed} </li>
-            <li><strong>Appearance: </strong> {item.appearance.map( (appear, index) => {
-              return (<>{appear}{index < item.appearance.length-1 ? "," : " "}  </>)
+            <li><strong>Actor: </strong> {portrayed} </li>
+            <li><strong>Show: </strong> {category} </li>
+            <li><strong>Appearance: </strong> {appearance.map( (appear, index) => {
+              return (<>{appear}{index < appearance.length-1 ? "," : " "}  </>)
             } )} </li>
-            <li><strong>Status: </strong> {item.status} </li>
+            <li><strong>Status: </strong> {status} </li>
           </ul>
         </div>
       </div>
